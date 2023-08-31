e523cfc1a125e9d501b8ca9a198f18475d4e4ff1
Leonardo Barros Leite <59918471+moscarpg@users.noreply.github.com>
2023-08-30 21:50:20 -0300

    Projeto

--- core/models.py
+++ core/models.py
@@ -11,5 +11,8 @@ class Corredor(models.Model):
 class Produto(models.Model):
     nome = models.CharField(max_length=50)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
-    imagem = models.ImageField(upload_to='imagens')
+    imagem = models.ImageField(upload_to='imagens/')
     corredor = models.ForeignKey(Corredor, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.nome
--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,4 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home')
-]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
